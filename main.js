ffa07505a1b265bfed71924a652a8b5949690780
benjirez <benjirez@gmail.com>
2018-01-16 21:35:25 -0800

    tuesday night 1/16/18

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-window.onload = function(){
-  var request_obj = new XMLHttpRequest(); // ajax object available in all modern browsers
-
-  request_obj.onreadystatechange = function(){
-    if(request_obj.readyState == 4 && request_obj.status == 200){
-      console.log( JSON.parse( request_obj.response ) );
-    }
-  }
-
-  request_obj.open('GET', 'data/dummy.json', true) // true for async (or false for sync)
-  request_obj.send();
-
-
-}
+console.log( __dirname +' and ' + __filename);
