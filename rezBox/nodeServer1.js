181725243edda0e57358c5f261c9008b046b3a2d
benjirez <benjirez@gmail.com>
2018-01-17 17:18:44 -0800

    added nodeServer1.js

---
+++ rezBox/nodeServer1.js
@@ -0,0 +1,12 @@
+// basic node server
+
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+	console.log('chewy was asked for the URL: ' + req.url); 
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('chewy was here');
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('chewy on 3000');
