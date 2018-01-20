846fa8d2963fd16d36500b63c1e94e7ebf02862f
ben resnick <benresnick@benjirez-imac.gateway.sonic.net>
2018-01-20 14:12:50 -0800

    hellz to the yeah, server streaming html in chunks

--- rezBox/nodeServer3.js
+++ rezBox/nodeServer3.js
@@ -5,9 +5,20 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
 	console.log('chewy was asked for the URL: ' + req.url); 
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write('server3');
-	res.end();
+
+	var myReadStream = fs.createReadStream(__dirname + '/../index.html', 'utf8');
+	var myDoc = '';
+
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	
+
+	myReadStream.on('data', function( chunk ){
+		console.log('chunk time');
+		//console.log( chunk );
+		res.write( chunk );
+		res.end(); // it would appear you write and end each chunk... ie. "written/sent" to browser in chunks too!
+	});
+
 });
 
 server.listen(3000, '127.0.0.1');
